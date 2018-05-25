f4cefd0d0039baa8ddc846c75bea78fdda700236
Miguel Alanis <anamelessdeath@gmail.com>
2018-05-24 19:54:41 -0500

    Adding data folder to current installation

--- src/buildPackage.bat
+++ src/buildPackage.bat
@@ -2,9 +2,11 @@ SET ver=v1.0
 rmdir /s /q ..\release\aura
 del /f /s /q ..\release\*
 mkdir ..\release\aura
+mkdir ..\release\aura\data
 dotnet publish
 xcopy /s /Y C:\Actica\Projects\tools\AuraSFTP\src\bin\Debug\netcoreapp2.0\publish\*.json ..\release\aura
 xcopy /s /Y C:\Actica\Projects\tools\AuraSFTP\src\bin\Debug\netcoreapp2.0\publish\*.dll ..\release\aura
+xcopy /s /Y C:\Actica\Projects\tools\AuraSFTP\src\data\* ..\release\aura\data
 xcopy /s /Y C:\Actica\Projects\tools\AuraSFTP\src\installer\* ..\release\
 REM Use echo to see preview of the command for /d %%a in (..\release) do (ECHO zip -r -p "..\release\aura_%version%.zip" ".\%%a\*")
 CD ..
