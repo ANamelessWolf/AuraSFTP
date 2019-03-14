99ae2d55253cce597ae6b59f82b26aa2abf9ebc3
Miguel Alanis <anamelessdeath@gmail.com>
2019-03-14 16:35:06 -0600

    Grammar function description fix

--- src/Model/MapDefinition.cs
+++ src/Model/MapDefinition.cs
@@ -9,11 +9,11 @@ namespace Nameless.Libraries.Aura.Model {
     /// </summary>
     public class MapDefinition {
         /// <summary>
-        /// Gets the mapped directories paths asociated to the SSH project
+        /// Gets the mapped directories paths associated to the SSH project
         /// </summary>
         public MappedPath[] Directories;
         /// <summary>
-        /// Gets the mapped files paths asociated to the SSH project
+        /// Gets the mapped files paths associated to the SSH project
         /// </summary>
         public MappedPath[] Files;
     }
